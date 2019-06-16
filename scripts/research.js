c9e42602c7a5b3cd3cd012634f748561e02352c6
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2019-06-16 16:10:45 -0700

    toggle working

--- scripts/research.js
+++ scripts/research.js
@@ -2,8 +2,4 @@ $().ready(function(){
     $(".readmore").click(function(){
       $('.toggleblurb').slideToggle();
     });
-
-    $(".readmore").click(function(){
-        console.log('clicked')
-    })
   });
